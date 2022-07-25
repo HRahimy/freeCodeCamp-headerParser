f91f43fa1a57a82374a61eb44800b9b938ad4162
Hamza Rahimy <rahimy.hamza@gmail.com>
2022-07-25 14:49:40 +0000

    Implement solution with tests passing

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ var app = express();
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -22,6 +27,16 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function (req, res) {
+  // Reference to solution for getting request language and software:
+  // https://stackoverflow.com/a/69945586/5472560
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
 });
 
 // listen for requests :)
